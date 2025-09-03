34c96e50352385d08712c788a1bb5f65bef7def6
Yaseakun p <agathagemink2014@gmail.com>
2025-09-03 10:35:16 +0000

    taoyao: Import vendor blobs from taoyao V816.0.23.0.ULIMIXM

---
+++ Android.mk
@@ -0,0 +1,28 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),taoyao)
+
+
+$(call add-radio-file-sha1-checked,radio/abl.img,b2a1ed56f07b39c74ebd859d69196e14d90d0ec9)
+$(call add-radio-file-sha1-checked,radio/aop.img,de65f96815089b893dad8fce9e1606dd601606e1)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,3dd99f3728a0d9dffb3402b0fc7f9999a040eada)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,21f237f43ec6c22fa567166a26cad56f3b8f5be5)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,c6861de931f19875c44cc497869a6c0f9fb73036)
+$(call add-radio-file-sha1-checked,radio/dsp.img,d3fdbfa43b8686991d86c6679c3a87b07a11b368)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,a4edbc8a1422c082637ad2218486bd85fd7c7dc7)
+$(call add-radio-file-sha1-checked,radio/hyp.img,0dcd83265248dc7bb9af9a6b808f8c85fffdad64)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,e0fb3ade49f3287f34364f48f4014edb13a05f81)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,0356900a8d0fdc305d563e816e7c72a41d441b55)
+$(call add-radio-file-sha1-checked,radio/modem.img,e1ddba1b5eb7e0ee85491aa28947da550a597eef)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,4547457ca63950f455fdbac8425cd266aaeb1813)
+$(call add-radio-file-sha1-checked,radio/shrm.img,6e78deca94d38b6aae99fff7c216bf89487f8509)
+$(call add-radio-file-sha1-checked,radio/tz.img,6c0103cb28838c2a88ca58bd82c63f4aac6caf96)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,c80dd2d5326f6381ddffaf78fde2a8780a55b389)
+$(call add-radio-file-sha1-checked,radio/xbl.img,0e1843621c1cbb37184e0767aaea49614d3c9291)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,c8451dc131213d3be2961c91a1487c32195ab5f8)
+
+endif
